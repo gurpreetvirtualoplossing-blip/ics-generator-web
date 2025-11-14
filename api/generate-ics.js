de49cfb8d929649fd2d64a2cf6ccc706aaf41270
gurpreetvirtualoplossing-blip <gurpreet.virtualoplossing@gmail.com>
2025-11-14 12:17:10 +0530

    Update generate-ics.js
    Final changes

--- api/generate-ics.js
+++ api/generate-ics.js
@@ -6,6 +6,19 @@ import Tesseract from "tesseract.js";
 
 export default async function handler(req, res) {
 
+  // ---------------------------
+  // 🔵 ADD CORS HEADERS (FIX)
+  // ---------------------------
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Preflight OPTIONS request
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+  // ---------------------------
+
   const file = req.body?.file;
   const text = req.body?.text;
   const url = req.body?.url;
